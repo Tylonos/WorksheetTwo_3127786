8e835f0860a8246deb17dc10434ffa33e8809f1e
ivan_novik <nvikivan280@gmail.com>
2025-03-03 18:00:37 +0000

    Stub methods added to the conversions.java

--- src/griffith/Conversions.java
+++ src/griffith/Conversions.java
@@ -1,4 +1,29 @@
 package griffith;
 
-public class Conversions {
-}
+    public class Conversions {
+
+        // Converts Euro to Dollar
+        public double euroToDollar(double euro) {
+            return 0.0;
+        }
+
+        // Converts Dollar to Euro
+        public double dollarToEuro(double dollar) {
+            return 0.0;
+        }
+
+        // Converts a String to an Integer
+        public int stringToInteger(String val) {
+            return 0;
+        }
+
+        // Converts an Integer to a String
+        public String integerToString(int val) {
+            return "";
+        }
+
+        // Switches case (uppercase to lowercase and vice versa)
+        public String switchCase() {
+            return "";
+        }
+    }
